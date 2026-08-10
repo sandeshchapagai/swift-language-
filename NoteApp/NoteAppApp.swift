af56e304092a4ac6346849921daf248616777773
Sandesh Chapagai <sandeshchapagai15@gmail.com>
2026-08-10 10:30:35 +0545

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Undefine on 10/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
